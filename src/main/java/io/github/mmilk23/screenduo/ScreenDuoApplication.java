70b6d0551215c949c2e626cd0aa9b330d7d63068
mmilk23 <5692175+mmilk23@users.noreply.github.com>
2026-09-06 23:11:29 -0300

    chore: initialize ScreenDUO Radar project

---
+++ src/main/java/io/github/mmilk23/screenduo/ScreenDuoApplication.java
@@ -0,0 +1,11 @@
+package io.github.mmilk23.screenduo;
+
+public final class ScreenDuoApplication {
+
+    private ScreenDuoApplication() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("ScreenDUO Radar");
+    }
+}
